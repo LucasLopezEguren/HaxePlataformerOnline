4327566b5050b8f5397eb185fc0e4e6df04dc115
lucasl <lucasl@oktana.io>
2020-07-14 09:23:28 -0300

    upload HTML5 build

---
+++ yo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="yo" tilewidth="185" tileheight="504" tilecount="1" columns="1">
+ <image source="yo.png" width="185" height="504"/>
+</tileset>
